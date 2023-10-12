650b60c3b2f06a9d5474494d459a5139f0d897e8
lchulsey <126140963+wowzowski@users.noreply.github.com>
2023-10-11 19:53:56 -0500

    bleh

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -1,21 +1,51 @@
 const express = require('express');
+const router = express.Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
-const apiRouter = express.Router();
-
-apiRouter.get('/notes', (req, res) => {
-  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
-  res.json(notes);
+// Read notes from the db.json file
+router.get('/notes', (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync('develop/db/db.json', 'utf8'));
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while reading notes.' });
+  }
 });
 
-apiRouter.post('/notes', (req, res) => {
-  const newNote = req.body;
-  newNote.id = uuidv4();
-  const notes = JSON.parse(fs.readFileSync('db.json', 'utf8'));
-  notes.push(newNote);
-  fs.writeFileSync('db.json', JSON.stringify(notes, null, 2));
-  res.json(newNote);
+// Create a new note and save it to the db.json file
+router.post('/notes', (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync('develop/db/db.json', 'utf8'));
+    const newNote = {
+      id: uuidv4(),
+      title: req.body.title,
+      text: req.body.text,
+    };
+
+    data.push(newNote);
+    fs.writeFileSync('develop/db/db.json', JSON.stringify(data, null, 2));
+    res.json(newNote);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while saving the note.' });
+  }
 });
 
-module.exports = apiRouter;
+// Delete a note with a specific ID
+router.delete('/notes/:id', (req, res) => {
+  try {
+    const data = JSON.parse(fs.readFileSync('develop/db/db.json', 'utf8'));
+    const noteId = req.params.id;
+
+    const updatedData = data.filter((note) => note.id !== noteId);
+    fs.writeFileSync('develop/db/db.json', JSON.stringify(updatedData, null, 2));
+    res.json({ message: 'Note deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while deleting the note.' });
+  }
+});
+
+module.exports = router;
--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -1,14 +1,15 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const router = express.Router();
 
-const htmlRouter = express.Router();
-
-htmlRouter.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
-
-htmlRouter.get('*', (req, res) => {
+// Route to serve the homepage (index.html)
+router.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-module.exports = htmlRouter;
+// Route to serve the notes page (notes.html)
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+module.exports = router;
