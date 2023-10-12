f39788129a32e9a606952c71190ad24e21436d47
lchulsey <126140963+wowzowski@users.noreply.github.com>
2023-10-11 19:55:47 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+const htmlRouter = require('./Routes/htmlRoutes');
+const apiRouter = require('./Routes/apiRoutes');
+
+app.use('/', htmlRouter);
+app.use('/api', apiRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
